c5a7ee7e05f497d2a2ca3d1bfff9d68f0664cc17
RybakovDmitry2072 <143866154+RybakovDmitry2072@users.noreply.github.com>
2025-04-23 22:19:41 +0300

    v.15

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -26,10 +26,12 @@ class CommentsViewSet(viewsets.ModelViewSet):
     def get_object(self):
         comment_id = self.kwargs['comment_id']
         post_id = self.kwargs['post_id']
-        comment = get_object_or_404(Comment, pk=comment_id, post_id=post_id)
+        comment = get_object_or_404(Comment, 
+                                    pk=comment_id,
+                                    post_id=post_id)
         self.check_object_permissions(self.request, comment)
         return comment
-        
+
     def get_queryset(self):
         post_id = self.kwargs['post_id']
         return Comment.objects.filter(post_id=post_id)
